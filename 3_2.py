6725658d8725e1ae62942b23c068636e45f64091
emily <1303currane@gmail.com>
2024-06-13 18:21:31 +0100

    removed small error in stack.py and solution to chap 3 quesiton 2

--- 3_2.py
+++ 3_2.py
@@ -1,9 +1,10 @@
 import pytest
+import random
 
-class Stack:
+class MinStack:
     class StackNode:
-        value = None
         nextNode = None
+        min = float('inf')
 
         def __init__(self, value):
             self.value = value
@@ -13,6 +14,16 @@ class Stack:
     def push(self, value):
         t = self.StackNode(value)
         t.next = self.top
+
+        #check to see if this value is smaller than previous min
+        if t.next == None:
+            t.min = t.value
+        else:
+            if t.value < t.next.min:
+                t.min = t.value
+            else:
+                t.min = t.next.min
+
         self.top = t
 
     def pop(self):
@@ -34,6 +45,12 @@ class Stack:
             my_arr.append(current_node.value)
             current_node = current_node.next
         print(my_arr)
+
+    def peek_min(self):
+        if(self.top == None):
+            raise StackEmptyError(f"can't get min of an empty stack")   
+        return self.top.min
+
     
 class StackError(Exception):
     """stack operation error"""
